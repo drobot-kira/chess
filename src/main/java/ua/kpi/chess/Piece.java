8ac632ffd28920593aa749c57c66ad2c0ab8923f
Artem <artemzinych888@gmail.com>
2025-03-30 23:06:32 +0300

    BoardClass

---
+++ src/main/java/ua/kpi/chess/Piece.java
@@ -0,0 +1,16 @@
+﻿package ua.kpi.chess;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Piece
+{
+    static public List<Byte> FindPossibleMoves(byte[][] field, byte coords)
+    {
+        List<Byte> list = new ArrayList<>();
+
+        list.add((byte)3); list.add((byte) 13); list.add((byte)23); list.add((byte)33); list.add((byte)40); list.add((byte)41); list.add((byte)42); list.add((byte)44); list.add((byte)45); list.add((byte)46); list.add((byte)53 );
+        return list;
+    }
+}
+
