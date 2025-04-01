95ed820b21474abd71205a047304b274f3da0776
Vadym-Hryhorash <hryhorashvo@gmail.com>
2025-04-01 17:00:45 +0300

    Fixed some issues.

--- src/main/java/ua/kpi/chess/Game.java
+++ src/main/java/ua/kpi/chess/Game.java
@@ -1,13 +1,40 @@
 package ua.kpi.chess;
-import java.io.File;
-import java.io.IOException;
-import java.io.FileWriter;
-import javax.swing.*;
 
-public class Game
-{
-    private byte[][] GetField(int GameId) //Дістати поле з БД за GameId
-    {
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Game {
+    private byte[][] getField(int GameId) {
+        List<byte[]> matrixList = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(
+                "C:\\Users\\Lenovo\\Desktop\\server\\Vadym\\src\\main\\java\\ua\\kpi\\chess\\field.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] numbers = line.trim().split("\\s+"); // Розбиваємо по пробілах
+                byte[] row = new byte[numbers.length];
+                for (int i = 0; i < numbers.length; i++) {
+                    row[i] = Byte.parseByte(numbers[i]); // Конвертуємо у byte
+                }
+                matrixList.add(row);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return matrixList.toArray(new byte[0][]);
+    }
+
+    private int getWhiteId(int GameId) {
+        return 1;
+    }
+
+    private void writeTheEnd() {
         byte[][] field = {
                 {24, 22, 23, 25, 26, 23, 22, 24},
                 {21, 21, 21, 21, 21, 21, 21, 21},
@@ -16,136 +43,143 @@ public class Game
                 {30, 30, 30, 30, 30, 30, 30, 30},
                 {30, 30, 30, 30, 30, 30, 30, 30},
                 {11, 11, 11, 11, 11, 11, 11, 11},
-                {14, 12, 13, 16, 15, 13, 12, 14},
-                { 1,  0,  0,  0,  0,  0,  0,  0}
-        };
-        return field;
-
+                {14, 12, 13, 15, 16, 13, 12, 14},
+                {1, 11, 11, 0, 0, 0, 0, 0}};
+        writeMove(1, field);
     }
 
-    /*private void GameEnded(byte end)
-    {
-
-    }*/
-
-    public byte[][] SquareClicked(int GameId, int SquareId, int UserId) {
-        byte[][] field = GetField(GameId); //Дістати поле з БД за GameId
-        int UserIdWhite = 105;   //Дістати з БД UserIdWhite
-        Board board = new Board();
-        int i = SquareId / 10;
-        int j = SquareId % 10;
-        field[i][j] = (byte) (field[i][j] + 100);
-        String temp = Integer.toString(SquareId);
-        byte squareId = Byte.parseByte(temp);
-        if (UserId == UserIdWhite && field[i][j]/10 == 1){
-            board.RemoveMarks(field);
-            board.AddMarks(field, squareId);
+    private void writeMove(int GameId, byte[][] field) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("field.txt"))) {
+            for (byte[] row : field) {
+                for (byte num : row) {
+                    writer.write(num + " ");  // Записуємо числа через пробіл
+                }
+                writer.newLine(); // Перехід на новий рядок
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        else{
-            Piece piece = new Piece();
-            if (board.IsThereAMarkedPiece(field) != -1){
-                if (piece.IsMovePossible(field, squareId)){
-                    int k = board.IsThereAMarkedPiece(field)/10;
-                    int l = board.IsThereAMarkedPiece(field)%10;
-                    if(field[i][j]%10 == 1 || field[k][l]%10 == 1){
+    }
+
+    public byte[][] SquareClicked(int GameId, byte SquareId, int UserId) {
+        byte[][] field = getField(GameId);
+        int WhiteId = getWhiteId(GameId);
+
+        byte i = (byte) (SquareId / 10);
+        byte j = (byte) (SquareId % 10);
+
+        if ((field[SquareId / 10][SquareId % 10] == 1 && WhiteId == UserId) ||
+                (field[SquareId / 10][SquareId % 10] == 2 && WhiteId != UserId)) // user's piece
+        {
+            field = Board.AddMarks(Board.RemoveMarks(field), SquareId);
+        } else // not user's piece
+        {
+            byte selectedPiece = Board.IsThereAMarkedPiece(field);
+            if (selectedPiece != -1) // there is a selected piece
+            {
+                if (field[SquareId / 10][SquareId % 10] < 0) // the move is possible
+                {
+                    field = Board.RemoveMarks(field);
+                    boolean willBeEnPassant = false;
+                    if (field[selectedPiece / 10][selectedPiece % 10] % 10 == 1) // pawn is moving
+                    {
+                        if (field[8][0] == 1) {
+                            if (SquareId == field[8][3]) {
+                                field[SquareId / 10 + 1][SquareId % 10] = 30;
+                            } else if (SquareId == selectedPiece - 20) {
+                                field[8][3] = (byte) (selectedPiece - 10);
+                                willBeEnPassant = true;
+                            }
+                        } else if (field[8][0] == 2) {
+                            if (SquareId == field[8][3]) {
+                                field[SquareId / 10 - 1][SquareId % 10] = 30;
+                            } else if (SquareId == selectedPiece + 20) {
+                                field[8][3] = (byte) (selectedPiece + 10);
+                                willBeEnPassant = true;
+                            }
+                        }
+                        // ****врахувати перетворення
+                    } else if (field[selectedPiece / 10][selectedPiece % 10] % 10 == 4) // rook is moving
+                    {
+                        if (field[8][0] / 10 == 1) {
+                            if (SquareId % 10 == 0 && field[8][1] >= 10) {
+                                field[8][1] -= 10;
+                            } else if (SquareId % 10 == 7 && field[8][1] % 10 >= 1) {
+                                field[8][1] -= 1;
+                            }
+                        } else {
+                            if (SquareId % 10 == 0 && field[8][2] >= 10) {
+                                field[8][2] -= 10;
+                            } else if (SquareId % 10 == 7 && field[8][2] % 10 >= 1) {
+                                field[8][2] -= 1;
+                            }
+                        }
+                    } else if (field[selectedPiece / 10][selectedPiece % 10] % 10 == 6) // king is moving
+                    {
+                        if (selectedPiece - 2 == SquareId) // long castle
+                        {
+                            field[selectedPiece / 10][selectedPiece % 10 - 1] = field[selectedPiece / 10][0];
+                            field[selectedPiece / 10][0] = 30;
+                        } else if (selectedPiece + 2 == SquareId) // short castle
+                        {
+                            field[selectedPiece / 10][selectedPiece % 10 + 1] = field[selectedPiece / 10][7];
+                            field[selectedPiece / 10][7] = 30;
+                        }
+
+                        if (field[8][0] == 1) {
+                            field[8][1] = 0;
+                        } else if (field[8][0] == 2) {
+                            field[8][2] = 0;
+                        }
+                    }
+                    field[i][j] = field[selectedPiece / 10][selectedPiece % 10];
+                    field[selectedPiece / 10][selectedPiece % 10] = 30;
+                    field[8][0] = field[8][0] == 1 ? (byte) 2 : (byte) 1;
+                    if (!willBeEnPassant) {
+                        field[8][3] = 0;
+                    }
+                    if (field[selectedPiece / 10][selectedPiece % 10] % 10 == 1 ||
+                            field[SquareId / 10][SquareId % 10] != 30) {
                         field[8][4] = 0;
+                    } else {
+                        field[8][4] += 1;
                     }
-                    else{
-                        field[8][4]++;
+                    if (field[8][0] == 2) {
+                        field[8][5] += 1;
                     }
-                    int index;
-                    if(UserId == UserIdWhite){
-                        index = 1;
-                    }
-                    else{
-                        index = 2;
-                    }
-                    if(board.IsThereAMarkedPiece(field) == 70){
-                        field[8][index] = 1;
-                    }
-                    else if(board.IsThereAMarkedPiece(field) == 77){
-                        field[8][index] = 10;
-                    }
-                    else if(board.IsThereAMarkedPiece(field) == 74){
-                        field[8][index] = 0;
-                    }
-                    if(field[i][j]%10 == 1){
-                        if(j - 1 < 0){
-                            if(field[i][j+1]%10==1 && field[i][j+1]/10!=field[i][j]/10){
-                                field[8][3] = (byte)(i*10+(j+1));
-                            }
-                        }
-                        else if(j + 1 > 7){
-                            if(field[i][j-1]%10==1 && field[i][j-1]/10!=field[i][j]/10){
-                                field[8][3] = (byte)(i*10+(j-1));
-                            }
-                        }
-                        else{
-                            if(field[i][j-1]%10==1 && field[i][j-1]/10!=field[i][j]/10){
-                                field[8][3] = (byte)(i*10+(j+1));
-                            }
-                            else if(field[i][j+1]%10==1 && field[i][j+1]/10!=field[i][j]/10){
-                                field[8][3] = (byte)(i*10+(j+1));
-                            }
-                        }
-                    }
-                    byte buff = field[k][l];
-                    field[k][l] = 30;
-                    field[i][j] = buff;
-                    field[i][j] = (byte)(buff - 100);
-                    board.RemoveMarks(field);
-                    if(UserId == UserIdWhite){
-                        field[8][0] = 2;
-                    }
-                    else{
-                        field[8][0] = 1;
-                        field[8][5]++;
-                    }
-                    Position position = new Position();
-                    if(position.IsGameEnded(field) != 0){
-                        for(int x = 0; x < 9; x++){
-                            for (int y = 0; y < 8; y++){
-                                field[x][y] = 0;
-                            }
-                        }
-                        field[0][0] = position.IsGameEnded(field);
-                        if(UserId == UserIdWhite){
-                            if (position.IsGameEnded(field) > 0){
-                                field[0][1] = 1;
-                            }
-                            else{
-                                field[0][1] = -1;
-                            }
-                        }
-                        else {
-                            if (position.IsGameEnded(field) > 0) {
-                                field[0][1] = -1;
+
+                    writeMove(GameId, field);
+
+                    byte theEnd = Position.IsGameEnded(field);
+                    if (theEnd != 0) // the game is ended
+                    {
+                        byte value = 0;
+                        if (Math.abs(theEnd) == 1 || Math.abs(theEnd) == 4) {
+                            if (theEnd > 0 && field[8][0] == 1 || theEnd < 0 && field[8][0] == 2) {
+                                value = 1;
                             } else {
-                                field[0][1] = 1;
+                                value = -1;
                             }
                         }
-                        try{
-                            FileWriter myWriter = new FileWriter("src\\main\\java\\ua\\kpi\\chess\\field.txt");
-                            for(i = 0; i < 9; i++){
-                                for(j = 0; j < 8; j++){
-                                    myWriter.write(field[i][j] + " ");
-                                }
-                                myWriter.write("\n");
+                        for (byte a = 0; a < 9; a++) {
+                            for (byte b = 0; b < 8; b++) {
+                                field[a][b] = 0;
                             }
-                            myWriter.write("UserIdWhite\n");
-                            myWriter.write("UserIdBlack");
-                            myWriter.close();
                         }
-                        catch(IOException e){
-                            System.out.println("Error");
-                        }
+                        field[0][0] = theEnd;
+                        field[0][1] = value;
+                        writeTheEnd();
                     }
+                } else // the move is not possible
+                {
+                    field = Board.RemoveMarks(field);
                 }
-                else{
-                    board.RemoveMarks(field);
-                }
+            } else // there is not a selected piece
+            {
+                field = Board.RemoveMarks(field);
             }
         }
+
         return field;
     }
 }
