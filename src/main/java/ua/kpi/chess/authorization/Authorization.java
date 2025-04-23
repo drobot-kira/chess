8d0809057f676eb5b3f0a5cad213b955011626bd
Artem <artemzinych888@gmail.com>
2025-04-23 23:07:29 +0300

    Added LogIn script #FDOTtQWZ

--- src/main/java/ua/kpi/chess/authorization/Authorization.java
+++ src/main/java/ua/kpi/chess/authorization/Authorization.java
@@ -1,9 +1,25 @@
-﻿package ua.kpi.chess.authorization;
+package ua.kpi.chess.authorization;
+
+import ua.kpi.chess.databaseinteraction.DatabaseHandler;
+import java.sql.ResultSet;
 
 public class Authorization {
 
     public static byte LogIn(String name, String password) {
-        return 1;
+        try {
+            DatabaseHandler dbHandler = new DatabaseHandler();
+            ResultSet result = dbHandler.getUser(name);
+            if(result.next()){
+                String correctPassword = result.getString("password");
+                if(password.equals(correctPassword)){
+                    return 1;
+                }
+            }
+            return -1;
+        }
+        catch (Exception e){
+            return 0;
+        }
     }
 
     public static byte SignUp(String name, String password) {
