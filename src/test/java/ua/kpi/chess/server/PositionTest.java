4dd4d45558d5a96aadc2e9353d3d0948b2a1d071
Bohdan2007 <bohdan.zhyzhka@gmail.com>
2025-05-28 23:11:52 +0300

    PositionTest

---
+++ src/test/java/ua/kpi/chess/server/PositionTest.java
@@ -0,0 +1,312 @@
+﻿package ua.kpi.chess.server;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyByte;
+
+public class PositionTest {
+    @Test
+    void testWhiteKingVsBlackKing() {
+        //Arrange
+        byte[][] field = {
+                {26, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {16, 30, 30, 30, 30, 30, 30, 30},
+                {1, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        try (MockedStatic<Piece> pieceMock = Mockito.mockStatic(Piece.class)) {
+            pieceMock.when(() -> Piece.IsThereACheck(any(), anyByte(), anyByte())).thenReturn(false);
+            boolean correct = true;
+            //Act
+            var position = new Position();
+            boolean test = position.IsThereAnInsufficientMaterial(field);
+            //Assert
+            assertEquals(correct, test);
+        }
+    }
+    @Test
+    void testWhiteKingAndKnightVsBlackKing() {
+        //Arrange
+        byte[][] field = {
+                {26, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {12, 30, 30, 30, 30, 30, 30, 30},
+                {16, 30, 30, 30, 30, 30, 30, 30},
+                {1, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        try (MockedStatic<Piece> pieceMock = Mockito.mockStatic(Piece.class)) {
+            pieceMock.when(() -> Piece.IsThereACheck(any(), anyByte(), anyByte())).thenReturn(false);
+            boolean correct = true;
+            //Act
+            var position = new Position();
+            boolean test = position.IsThereAnInsufficientMaterial(field);
+            //Assert
+            assertEquals(correct, test);
+        }
+    }
+    @Test
+    void testWhiteKingAndKnightAndKnightVsBlackKing() {
+        //Arrange
+        byte[][] field = {
+                {26, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {12, 12, 30, 30, 30, 30, 30, 30},
+                {16, 30, 30, 30, 30, 30, 30, 30},
+                {1, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        try (MockedStatic<Piece> pieceMock = Mockito.mockStatic(Piece.class)) {
+            pieceMock.when(() -> Piece.IsThereACheck(any(), anyByte(), anyByte())).thenReturn(false);
+            boolean correct = true;
+            //Act
+            var position = new Position();
+            boolean test = position.IsThereAnInsufficientMaterial(field);
+            //Assert
+            assertEquals(correct, test);
+        }
+    }
+    @Test
+    void testWhiteKingAndBishopVsBlackKing() {
+        //Arrange
+        byte[][] field = {
+                {26, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {13, 30, 30, 30, 30, 30, 30, 30},
+                {16, 30, 30, 30, 30, 30, 30, 30},
+                {1, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        try (MockedStatic<Piece> pieceMock = Mockito.mockStatic(Piece.class)) {
+            pieceMock.when(() -> Piece.IsThereACheck(any(), anyByte(), anyByte())).thenReturn(false);
+            boolean correct = true;
+            //Act
+            var position = new Position();
+            boolean test = position.IsThereAnInsufficientMaterial(field);
+            //Assert
+            assertEquals(correct, test);
+        }
+    }
+    @Test
+    void testWhiteKingAndBishopAndKnightVsBlackKing() {
+        //Arrange
+        byte[][] field = {
+                {26, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {13, 12, 30, 30, 30, 30, 30, 30},
+                {16, 30, 30, 30, 30, 30, 30, 30},
+                {1, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        try (MockedStatic<Piece> pieceMock = Mockito.mockStatic(Piece.class)) {
+            pieceMock.when(() -> Piece.IsThereACheck(any(), anyByte(), anyByte())).thenReturn(false);
+            boolean correct = true;
+            //Act
+            var position = new Position();
+            boolean test = position.IsThereAnInsufficientMaterial(field);
+            //Assert
+            assertEquals(correct, test);
+        }
+    }
+    @Test
+    void testWhiteKingAndBishopVsBlackKingAndBishopWhiteField() {
+        //Arrange
+        byte[][] field = {
+                {26, 30, 30, 30, 30, 30, 13, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 23},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {16, 30, 30, 30, 30, 30, 30, 30},
+                {1, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        try (MockedStatic<Piece> pieceMock = Mockito.mockStatic(Piece.class)) {
+            pieceMock.when(() -> Piece.IsThereACheck(any(), anyByte(), anyByte())).thenReturn(false);
+            boolean correct = true;
+            //Act
+            var position = new Position();
+            boolean test = position.IsThereAnInsufficientMaterial(field);
+            //Assert
+            assertEquals(correct, test);
+        }
+    }
+    @Test
+    void testWhiteKingAndBishopVsBlackKingAndBishopDifferentField() {
+        //Arrange
+        byte[][] field = {
+                {26, 30, 30, 30, 30, 30, 13, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 23},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {16, 30, 30, 30, 30, 30, 30, 30},
+                {1, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        try (MockedStatic<Piece> pieceMock = Mockito.mockStatic(Piece.class)) {
+            pieceMock.when(() -> Piece.IsThereACheck(any(), anyByte(), anyByte())).thenReturn(false);
+            boolean correct = true;
+            //Act
+            var position = new Position();
+            boolean test = position.IsThereAnInsufficientMaterial(field);
+            //Assert
+            assertEquals(correct, test);
+        }
+    }
+    @Test
+    void testWhiteKingVsBlackKingAndKnight() {
+        //Arrange
+        byte[][] field = {
+                {26, 30, 30, 30, 30, 30, 30, 30},
+                {22, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {16, 30, 30, 30, 30, 30, 30, 30},
+                {1, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        try (MockedStatic<Piece> pieceMock = Mockito.mockStatic(Piece.class)) {
+            pieceMock.when(() -> Piece.IsThereACheck(any(), anyByte(), anyByte())).thenReturn(false);
+            boolean correct = true;
+            //Act
+            var position = new Position();
+            boolean test = position.IsThereAnInsufficientMaterial(field);
+            //Assert
+            assertEquals(correct, test);
+        }
+    }
+    @Test
+    void testWhiteKingVsBlackKingAndKnightAndKnight() {
+        //Arrange
+        byte[][] field = {
+                {26, 30, 30, 30, 30, 30, 30, 30},
+                {22, 22, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 12, 30, 30, 30, 30, 30, 30},
+                {16, 30, 30, 30, 30, 30, 30, 30},
+                {1, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        try (MockedStatic<Piece> pieceMock = Mockito.mockStatic(Piece.class)) {
+            pieceMock.when(() -> Piece.IsThereACheck(any(), anyByte(), anyByte())).thenReturn(false);
+            boolean correct = true;
+            //Act
+            var position = new Position();
+            boolean test = position.IsThereAnInsufficientMaterial(field);
+            //Assert
+            assertEquals(correct, test);
+        }
+    }
+    @Test
+    void testWhiteKingVsBlackKingAndBishop() {
+        //Arrange
+        byte[][] field = {
+                {26, 30, 30, 30, 30, 30, 30, 30},
+                {23, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {16, 30, 30, 30, 30, 30, 30, 30},
+                {1, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        try (MockedStatic<Piece> pieceMock = Mockito.mockStatic(Piece.class)) {
+            pieceMock.when(() -> Piece.IsThereACheck(any(), anyByte(), anyByte())).thenReturn(false);
+            boolean correct = true;
+            //Act
+            var position = new Position();
+            boolean test = position.IsThereAnInsufficientMaterial(field);
+            //Assert
+            assertEquals(correct, test);
+        }
+    }
+    @Test
+    void testWhiteKingVsBlackKingAndBishopAndKnight() {
+        //Arrange
+        byte[][] field = {
+                {26, 30, 30, 30, 30, 30, 30, 30},
+                {22, 23, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {16, 30, 30, 30, 30, 30, 30, 30},
+                {1, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        try (MockedStatic<Piece> pieceMock = Mockito.mockStatic(Piece.class)) {
+            pieceMock.when(() -> Piece.IsThereACheck(any(), anyByte(), anyByte())).thenReturn(false);
+            boolean correct = true;
+            //Act
+            var position = new Position();
+            boolean test = position.IsThereAnInsufficientMaterial(field);
+            //Assert
+            assertEquals(correct, test);
+        }
+    }
+    @Test
+    void testWhiteKingAndBishopVsBlackKingAndBishopBlackField() {
+        //Arrange
+        byte[][] field = {
+                {26, 23, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {30, 30, 30, 30, 30, 30, 30, 30},
+                {16, 30, 30, 30, 30, 30, 13, 30},
+                {1, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        try (MockedStatic<Piece> pieceMock = Mockito.mockStatic(Piece.class)) {
+            pieceMock.when(() -> Piece.IsThereACheck(any(), anyByte(), anyByte())).thenReturn(false);
+            boolean correct = true;
+            //Act
+            var position = new Position();
+            boolean test = position.IsThereAnInsufficientMaterial(field);
+            //Assert
+            assertEquals(correct, test);
+        }
+    }
+}
