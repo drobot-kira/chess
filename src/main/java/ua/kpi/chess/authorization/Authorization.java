3b4f8da465c866b7bd9135d01392fdfbb12e52b9
Artem <artemzinych888@gmail.com>
2025-05-31 01:25:19 +0300

    Added Logs. #eBreJFoJ #minor

--- src/main/java/ua/kpi/chess/authorization/Authorization.java
+++ src/main/java/ua/kpi/chess/authorization/Authorization.java
@@ -1,9 +1,12 @@
 package ua.kpi.chess.authorization;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ua.kpi.chess.databaseinteraction.DatabaseHandler;
 import java.sql.ResultSet;
 
 public class Authorization {
+    private static Logger logger = LoggerFactory.getLogger(Authorization.class);
 
     public static byte LogIn(String name, String password) {
         try {
@@ -12,9 +15,11 @@ public class Authorization {
             if(result.next()){
                 String correctPassword = result.getString("password");
                 if(password.equals(correctPassword)){
+                    logger.info("Successful log in");
                     return 1;
                 }
             }
+            logger.warn("Unsuccessful log in");
             return -1;
         }
         catch (Exception e){
@@ -27,9 +32,11 @@ public class Authorization {
         ResultSet result = dbHandler.GetUser(name);
         try {
             if (result.next()) {
+                logger.warn("Unsuccessful registration");
                 return -1;
             }
             dbHandler.AddUser(name, password);
+            logger.info("Successful registration");
             return 1;
         } catch (Exception e) {
             return 0;
