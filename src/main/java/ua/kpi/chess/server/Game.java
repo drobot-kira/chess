27e5abdc6bbd42a3a8c8b06c08430c6d53b8c2f4
Kira Drobot <kiradrobot1307@gmail.com>
2025-05-31 08:34:32 +0300

    Restored

--- src/main/java/ua/kpi/chess/server/Game.java
+++ src/main/java/ua/kpi/chess/server/Game.java
@@ -17,40 +17,129 @@ public class Game {
             { 1, 11, 11,  0,  0,  0,  0,  0}};
 
         DatabaseHandler dbFieldString = new DatabaseHandler();
-        String fieldString = dbFieldString.GetMoves(GameId);
+        String moves = dbFieldString.GetMoves(GameId);
 
-        if(fieldString.length() < 4){
+        int index = 0;
+        if (moves.charAt(index) == ' ')
+        {
+            index++;
+        }
+        while (index < moves.length())
+        {
+            if (moves.charAt(index) == '*')
+            {
+                break;
+            }
+            byte iPrev = (byte)(8 - (byte)(moves.charAt(index + 1)) + '0');
+            byte jPrev = (byte)((byte)(moves.charAt(index)) - 97);
+            byte iNext = (byte)(8 - (byte)(moves.charAt(index + 4)) + '0');
+            byte jNext = (byte)((byte)(moves.charAt(index + 3)) - 97);
+            byte selectedPiece = (byte)(iPrev * 10 + jPrev);
+            byte SquareId = (byte)(iNext * 10 + jNext);
+
+            boolean willBeEnPassant = false;
+            if (field[selectedPiece / 10][selectedPiece % 10] % 10 == 1) // pawn is moving
+            {
+                if (field[8][0] == 1) {
+                    if (SquareId == field[8][3]) {
+                        field[SquareId / 10 + 1][SquareId % 10] = 30;
+                    } else if (SquareId == selectedPiece - 20) {
+                        field[8][3] = (byte) (selectedPiece - 10);
+                        willBeEnPassant = true;
+                    }
+                } else if (field[8][0] == 2) {
+                    if (SquareId == field[8][3]) {
+                        field[SquareId / 10 - 1][SquareId % 10] = 30;
+                    } else if (SquareId == selectedPiece + 20) {
+                        field[8][3] = (byte) (selectedPiece + 10);
+                        willBeEnPassant = true;
+                    }
+                }
+            } else if (field[selectedPiece / 10][selectedPiece % 10] % 10 == 4) // rook is moving
+            {
+                if (field[8][0] / 10 == 1) {
+                    if (SquareId % 10 == 0 && field[8][1] >= 10) {
+                        field[8][1] -= 10;
+                    } else if (SquareId % 10 == 7 && field[8][1] % 10 >= 1) {
+                        field[8][1] -= 1;
+                    }
+                } else {
+                    if (SquareId % 10 == 0 && field[8][2] >= 10) {
+                        field[8][2] -= 10;
+                    } else if (SquareId % 10 == 7 && field[8][2] % 10 >= 1) {
+                        field[8][2] -= 1;
+                    }
+                }
+            } else if (field[selectedPiece / 10][selectedPiece % 10] % 10 == 6) // king is moving
+            {
+                if (selectedPiece - 2 == SquareId) // long castle
+                {
+                    field[selectedPiece / 10][selectedPiece % 10 - 1] = field[selectedPiece / 10][0];
+                    field[selectedPiece / 10][0] = 30;
+                } else if (selectedPiece + 2 == SquareId) // short castle
+                {
+                    field[selectedPiece / 10][selectedPiece % 10 + 1] = field[selectedPiece / 10][7];
+                    field[selectedPiece / 10][7] = 30;
+                }
+
+                if (field[8][0] == 1) {
+                    field[8][1] = 0;
+                } else if (field[8][0] == 2) {
+                    field[8][2] = 0;
+                }
+            }
+            field[iNext][jNext] = field[selectedPiece / 10][selectedPiece % 10];
+            field[selectedPiece / 10][selectedPiece % 10] = 30;
+            field[8][0] = field[8][0] == 1 ? (byte) 2 : (byte) 1;
+            if (!willBeEnPassant) {
+                field[8][3] = 0;
+            }
+            if (field[selectedPiece / 10][selectedPiece % 10] % 10 == 1 ||
+                    field[SquareId / 10][SquareId % 10] != 30) {
+                field[8][4] = 0;
+            } else {
+                field[8][4] += 1;
+            }
+            if (field[8][0] == 2) {
+                field[8][5] += 1;
+            }
+            index += 6;
+            if (moves.charAt(index - 1) == '*')
+            {
+                index--;
+                break;
+            }
+        }
+        if(moves.length() < 4){
             return field;
         }
-        int index = 0;
-        while(fieldString.charAt(index) != '*'){
-            byte iPrev = (byte)(8 - (byte)(fieldString.charAt(index + 1)) + '0');
-            byte jPrev = (byte)((byte)(fieldString.charAt(index)) - 97);
-            byte iNext = (byte)(8 - (byte)(fieldString.charAt(index + 4)) + '0');
-            byte jNext = (byte)((byte)(fieldString.charAt(index + 3)) - 97);
-
-            field[iNext][jNext] = field[iPrev][jPrev];
-            field[iPrev][jPrev] = 30;
-
-            index += 6;
-        }
-
         index++;
-        while(fieldString.charAt(index) != '*'){
-            byte iCurrent = (byte)(8 - (byte)(fieldString.charAt(index + 1)) + '0');
-            byte jCurrent = (byte)((byte)(fieldString.charAt(index)) - 97);
+        while(moves.charAt(index) != '*'){
+            byte iCurrent = (byte)(8 - (byte)(moves.charAt(index + 1)) + '0');
+            byte jCurrent = (byte)((byte)(moves.charAt(index)) - 97);
 
             field[iCurrent][jCurrent] = (byte)(field[iCurrent][jCurrent] * -1);
 
+            if (moves.charAt(index+2) == '*' || moves.charAt(index+1) == '*')
+            {
+                break;
+            }
             index += 3;
         }
 
-        index++;
-        if(fieldString.charAt(index) != '*'){
-            byte iCurrent = (byte)(8 - (byte)(fieldString.charAt(index + 1)) + '0');
-            byte jCurrent = (byte)((byte)(fieldString.charAt(index)) - 97);
+        index = Math.min(index + 3, moves.length() - 1);
+        if(moves.charAt(index) != '*'){
+            if(moves.charAt(index + 1) != '*') {
+                byte iCurrent = (byte) ((moves.charAt(index)) - '0');
+                byte jCurrent = (byte) (moves.charAt(index + 1) - '0');
 
-            field[iCurrent][jCurrent] += 100;
+                field[iCurrent][jCurrent] += 100;
+            }
+            else {
+                byte iCurrent = 0;
+                byte jCurrent = (byte) (moves.charAt(index) - '0');
+                field[iCurrent][jCurrent] += 100;
+            }
         }
 
         return field;
@@ -61,56 +150,68 @@ public class Game {
         String whiteName = dbHandler.GetWhiteId(GameId);
         return whiteName;
     }
-    public void writeMove(int GameId, byte[][] field, byte PieceCoords, byte SquareCoords ) {
-        DatabaseHandler dbFieldString = new DatabaseHandler();
-        String fieldString = dbFieldString.GetMoves(GameId);
-        String newFieldString = "";
+    public void WriteMove(int GameId, byte PieceCoords, byte SquareCoords ) {
+        DatabaseHandler databaseHandler = new DatabaseHandler();
+        String oldMoves = databaseHandler.GetMoves(GameId);
+        String newMoves = "";
 
-        for(int i = 0; i < fieldString.length(); i++){
-            if(fieldString.charAt(i) == '*'){
-                newFieldString = " ";
+        int index = 0;
+        boolean movesIsWritten = false;
+        while (index < oldMoves.length()) {
+            if (oldMoves.charAt(index) == '*' && !movesIsWritten) {
+                movesIsWritten = true;
+                byte iCoords = (byte) (8 - PieceCoords / 10);
+                char jCoords = (char) (PieceCoords % 10 + 97);
+                byte iNewCoords = (byte) (8 - SquareCoords / 10);
+                char jNewCoords = (char) (SquareCoords % 10 + 97);
+                newMoves += " " + jCoords + iCoords + "-" + jNewCoords + iNewCoords + "*";
+            }
+            newMoves += oldMoves.charAt(index);
+            index++;
+        }
+        databaseHandler.WriteMoves(GameId, newMoves);
+    }
+
+    public void WriteMarks(int GameId, byte[][] field)
+    {
+        DatabaseHandler databaseHandler = new DatabaseHandler();
+        String oldMoves = databaseHandler.GetMoves(GameId);
+        String newMoves = "";
+        int index = 0;
+        while (index < oldMoves.length()) {
+            if (oldMoves.charAt(index) == '*')
+            {
+                newMoves += "*";
                 break;
             }
-            newFieldString += fieldString.charAt(i);
+            newMoves += oldMoves.charAt(index);
+            index++;
         }
-
-        byte iCoords = (byte)(8 - PieceCoords/10);
-        char jCoords = (char)(PieceCoords%10 + 97);
-        byte iNewCoords = (byte)(8 - SquareCoords/10);
-        char jNewCoords = (char)(SquareCoords%10 + 97);
-
-        if(PieceCoords != SquareCoords){
-            newFieldString += jCoords;
-            newFieldString += iCoords;
-            newFieldString += "-";
-            newFieldString += iNewCoords;
-            newFieldString += jNewCoords;
-        }
-
-        newFieldString += " *";
-
-        String byf = "";
-        for(byte i = 0; i < field.length; i++){
-            for(byte j = 0; j < field[i].length; j++){
-                if(field[i][j] < 0){
-                    iCoords = (byte)(8 - i);
-                    jCoords = (char)(j + 97);
-                    newFieldString += jCoords;
-                    newFieldString += iCoords + " ";
-                }
-                else if(field[i][j] > 100){
-                    iCoords = (byte)(8 - i);
-                    jCoords = (char)(j + 97);
-                    byf += jCoords;
-                    byf += iCoords;
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (field[i][j] < 0)
+                {
+                    byte iCoords = (byte) (8 - i);
+                    char jCoords = (char) (j % 10 + 97);
+                    if (newMoves.charAt(newMoves.length() - 1) == '*')
+                    {
+                        newMoves += jCoords;
+                        newMoves += iCoords;
+                    }
+                    else {
+                        newMoves += " " + jCoords + iCoords;
+                    }
                 }
             }
         }
-
-        newFieldString += "*" + byf + "*";
-
-        DatabaseHandler dbUpdateFieldString = new DatabaseHandler();
-        dbUpdateFieldString.WriteMoves(GameId, newFieldString);
+        newMoves += "*";
+        byte selected = Board.IsThereAMarkedPiece(field);
+        if (selected != -1)
+        {
+            newMoves += selected;
+        }
+        newMoves += "*";
+        databaseHandler.WriteMoves(GameId, newMoves);
     }
 
     public byte[][] SquareClicked(int GameId, byte SquareId, String UserId) {
@@ -120,8 +221,13 @@ public class Game {
         byte i = (byte) (SquareId / 10);
         byte j = (byte) (SquareId % 10);
 
-        if ((field[SquareId / 10][SquareId % 10] / 10 % 10 == 1 && WhiteId == UserId) ||
-                (field[SquareId / 10][SquareId % 10] / 10 % 10 == 2 && WhiteId != UserId)) // user's piece
+        if (field[8][0] == 1 && !UserId.equals(WhiteId) || field[8][0] == 2 && UserId.equals(WhiteId))
+        {
+            return field;
+        }
+
+        if ((field[SquareId / 10][SquareId % 10] / 10 % 10 == 1 && WhiteId.equals(UserId)) ||
+                (field[SquareId / 10][SquareId % 10] / 10 % 10 == 2 && !WhiteId.equals(UserId))) // user's piece
         {
             field = Board.AddMarks(Board.RemoveMarks(field), SquareId);
         } else // not user's piece
@@ -131,6 +237,9 @@ public class Game {
             {
                 if (field[SquareId / 10][SquareId % 10] < 0) // the move is possible
                 {
+                    if (Board.IsThereAMarkedPiece(field) != -1) {
+                        WriteMove(GameId, Board.IsThereAMarkedPiece(field), SquareId);
+                    }
                     field = Board.RemoveMarks(field);
                     boolean willBeEnPassant = false;
                     if (field[selectedPiece / 10][selectedPiece % 10] % 10 == 1) // pawn is moving
@@ -210,21 +319,8 @@ public class Game {
                     byte theEnd = Position.IsGameEnded(fieldCopy, GameId);
                     if (theEnd != 0) // the server is ended
                     {
-                        byte value = 0;
-                        if (Math.abs(theEnd) == 1 || Math.abs(theEnd) == 4) {
-                            if (theEnd > 0 && field[8][0] == 1 || theEnd < 0 && field[8][0] == 2) {
-                                value = 1;
-                            } else {
-                                value = -1;
-                            }
-                        }
-                        for (byte a = 0; a < 9; a++) {
-                            for (byte b = 0; b < 8; b++) {
-                                field[a][b] = 0;
-                            }
-                        }
-                        field[0][0] = theEnd;
-                        field[0][1] = value;
+                        DatabaseHandler databaseHandler = new DatabaseHandler();
+                        databaseHandler.EndGame(GameId, theEnd);
                     }
                 } else // the move is not possible
                 {
@@ -241,8 +337,8 @@ public class Game {
         {
             PieceCoords = SquareId;
         }
-        writeMove(GameId, field, PieceCoords, SquareId);
 
+        WriteMarks(GameId, field);
         return field;
     }
 }
---
+++ src/main/resources/static/scripts/SpectatorLobby.js
@@ -0,0 +1,65 @@
+﻿let socket = new WebSocket("ws://localhost:8080/chess");
+
+const userName = localStorage.getItem('username');
+socket.onopen = () => {
+    socket.send(JSON.stringify({Type: "Identify", UserName: userName}));
+
+    let message = JSON.stringify({Type: "GetUnstartedGames"});
+    socket.send(message);
+}
+
+function renderGameList(games) {
+    gameList.innerHTML = '';
+    games.forEach(addGameToList);
+}
+
+function addGameToList(gameArray) {
+    const [id, username, timeControl] = gameArray;
+
+    const li = document.createElement('li');
+    li.className = 'game-container__item';
+    li.dataset.gameId = id;
+
+    const opponentDiv = document.createElement('div');
+    opponentDiv.className = 'opponent';
+    opponentDiv.textContent = username;
+
+    const containerDiv = document.createElement('div');
+    containerDiv.className = 'container-for-time-and-button';
+
+    const timeDiv = document.createElement('div');
+    timeDiv.className = 'container-for-time-and-button__time';
+    timeDiv.textContent = timeControl;
+
+    const button = document.createElement('button');
+    button.className = 'container-for-time-and-button__join-game-button';
+    button.textContent = 'Грати';
+    button.id = id;
+    button.onclick = () => joinGame(id);
+
+    containerDiv.appendChild(timeDiv);
+    containerDiv.appendChild(button);
+
+    li.appendChild(opponentDiv);
+    li.appendChild(containerDiv);
+
+    gameList.appendChild(li);
+}
+
+function joinGame(gameId) {
+    let message = JSON.stringify({Type: "StartGame", GameId: gameId, UserName: userName});
+    socket.send(message);
+}
+
+socket.onmessage = (event) => {
+    const data = JSON.parse(event.data);
+
+    if (data.type === 'AvailableGames') {
+        renderGameList(data.games);
+    } else if (data.type === 'StartGame') {
+        localStorage.setItem('color', data.color);
+        localStorage.setItem('gameid', data.gameid);
+        localStorage.setItem('opponent', data.opponent)
+        window.location.replace('/chess/game/index.html');
+    }
+};
